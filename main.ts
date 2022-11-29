61a3bf87be03fd4a0d4d473bc667fad9d9965c54
mrfortin <raphael.fortin@ecsd.net>
2022-11-29 10:08:48 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -147,4 +147,4 @@ scene.setBackgroundImage(img`
 tiles.setCurrentTilemap(tilemap`level1`)
 scene.cameraFollowSprite(mySprite)
 controller.moveSprite(mySprite, 100, 0)
-mySprite.ay = 80
+mySprite.ay = 90
